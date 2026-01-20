165b673994ed9bda9d6702b838d4f3f8b90da436
Vali <valireyimbergenov79@gmail.com>
2026-01-20 20:34:29 +0500

    EduAssistant - Automated exam and assignment control bot for teachers

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -3,7 +3,6 @@ import { ConfigService } from '@nestjs/config';
 import { config } from 'dotenv';
 
 config();
-
 const configService = new ConfigService();
 
 export const dataSourceOptions: DataSourceOptions = {
